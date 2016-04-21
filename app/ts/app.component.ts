6209988e5537c2703c981559575ac539990fd4b1
Daniel Kucal <dkucal@gmail.com>
2016-04-21 17:22:57 +0200

    angular2-translate init

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,9 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ProjectsService} from './projects.service';
 import {ProjectsComponent} from './projects.component';
 import {LanguagePickerComponent} from "./language-picker.component";
+import {TranslatePipe, TranslateService} from "ng2-translate/ng2-translate";
 
 @Component({
     selector: 'angular-app',
@@ -14,14 +15,30 @@ import {LanguagePickerComponent} from "./language-picker.component";
     directives: [
         ProjectsComponent,
         LanguagePickerComponent
+    ],
+    pipes: [
+        TranslatePipe
     ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     public languages = {
         eng: "English",
         pl: "Polski"
     };
     public chosenLang:String = null;
+
+    constructor(private _translate: TranslateService) { }
+
+    ngOnInit() {
+        var userLang = navigator.language.split('-')[0]; // use navigator lang if available
+        if (!this.languages.hasOwnProperty(userLang)) {
+            userLang = 'en';
+        }
+        // this language will be used as a fallback when a translation isn't found in the current language
+        this._translate.setDefaultLang('en');
+        // the lang to use, if the lang isn't available, it will use the current loader to get them
+        this._translate.use(userLang);
+    }
 
     changeLang(newLang):void {
         this.chosenLang = newLang;
--- app/ts/main.ts
+++ app/ts/main.ts
@@ -1,5 +1,10 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import 'rxjs/Rx';
 import {AppComponent} from './app.component';
+import {HTTP_PROVIDERS} from "angular2/http";
+import {TRANSLATE_PROVIDERS} from "ng2-translate/ng2-translate";
 
-bootstrap(<any> AppComponent);
+bootstrap(AppComponent, [
+    HTTP_PROVIDERS,
+    TRANSLATE_PROVIDERS
+]);
