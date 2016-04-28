1b9ef5d203976c83dc561e653c392ca5db85ff95
Daniel Kucal <dkucal@gmail.com>
2016-04-28 17:47:03 +0200

    enabled production mode

--- app/ts/main.ts
+++ app/ts/main.ts
@@ -3,7 +3,9 @@ import 'rxjs/Rx';
 import {AppComponent} from './app.component';
 import {HTTP_PROVIDERS} from "angular2/http";
 import {TRANSLATE_PROVIDERS} from "ng2-translate/ng2-translate";
+import {enableProdMode} from "angular2/core";
 
+enableProdMode();
 bootstrap(AppComponent, [
     HTTP_PROVIDERS,
     TRANSLATE_PROVIDERS
